6569ea34148b47d87623702c23edf3b272fc865b
Chuck D'Antonio <c_dantonio@acm.org>
2017-03-06 14:59:09 -0500

    Enabled the client to connect to non-default host/port with command-line arguments

--- src/main/ruby/client.rb
+++ src/main/ruby/client.rb
@@ -4,6 +4,25 @@ require './customer.pb'
 require 'net/http'
 require 'uri'
 
-uri = URI.parse('http://localhost:8080/customers/3')
+require 'optparse'
+require 'ostruct'
+
+options = OpenStruct.new
+options.host = "localhost"
+options.port = 8080
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: example.rb [options]"
+
+  opts.on("-h [HOST]", "--host [HOST]", "Hostname for the test service") do |host|
+    options.host = host
+  end
+
+  opts.on("-p [PORT]", "--port [PORT]", "Port for the test service") do |port|
+    options.port = port
+  end
+end.parse!
+
+uri = URI.parse("http://#{options.host}:#{options.port}/customers/3")
 body = Net::HTTP.get(uri)
 puts Demo::Customer.parse(body)
